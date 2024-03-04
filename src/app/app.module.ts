a60daeed68e1eed249ce4dcbc114ae5b7de9a8d1
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-03-04 19:24:46 +0300

    slider component design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
