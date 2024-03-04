a3b41e840e363caf1a17b0a51d8809a20203f32f
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-03-04 13:41:21 +0300

    toggle tabs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  activeItem: string = 'Now playing';
 
+  setActive(itemName) {
+    this.activeItem = itemName;
+  }
 }
