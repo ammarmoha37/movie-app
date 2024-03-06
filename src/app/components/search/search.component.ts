2240131886a03e0e856b24e3fa56b18911457165
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-03-07 02:02:11 +0300

    no result on search design

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  isSearch: boolean = true;
+
+  onSearch() {
+    this.isSearch = !this.isSearch;
+    console.log(this.isSearch);
+
+  }
 
 }
