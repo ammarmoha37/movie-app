8ada07cd5f8eebdf6467bfcb909b2f6514275866
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-03-09 19:19:32 +0300

    rating section : design

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class MovieDetailsComponent {
   activeItem: string = 'About Movie';
+  isRating: boolean = false;
 
   setActive(itemName) {
     this.activeItem = itemName;
+  }
+
+  switchRating() {
+    this.isRating = !this.isRating;
+  }
+
+  updateSliderBackground(event: Event) {
+    const slider: any = event.target as HTMLInputElement;
+    const percentage = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+    slider.style.background =
+      `linear-gradient(
+        to right, #FF8700 0%, #FF8700 ${percentage}%, #D9DBE9 ${percentage}%, #D9DBE9 100%)`;
   }
 }
