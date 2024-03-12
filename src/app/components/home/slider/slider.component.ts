8d24af8e7ca2a5c96270cda9e0393f4b16974d60
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-03-12 20:21:00 +0300

    slider linking with API

--- src/app/components/home/slider/slider.component.ts
+++ src/app/components/home/slider/slider.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../../models/movie.model';
+import { MovieService } from '../../../services/movie.service';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+  movies: Movie[];
 
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.fetchMoviesByCategory('top_rated')
+  }
+
+  fetchMoviesByCategory(category: string) {
+    this.movieService.getMoviesByCategory(category)
+    .subscribe((data: any) => {
+      this.movies = data.results.map((movie: any) => ({
+        id: movie.id,
+        title: movie.title,
+        img: this.movieService.getMovieImageUrl(movie.poster_path)
+      }));
+    });
+  }
 }
